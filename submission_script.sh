aef134d29a6afc6fd002927708bdce3076580065
Jason Hou <jason.kumi@gmail.com>
2021-11-05 11:31:56 -0400

    Update submission_script.sh

--- submission_script.sh
+++ submission_script.sh
@@ -2,9 +2,9 @@
 
 #SBATCH -t 30:00:00                    # Walltime
 #SBATCH -N 1                           # Number of nodes
-#SBATCH -n 8                          # Number of processor cores (i.e. tasks)
-#SBATCH -J "MOF"               # Job name
-#SBATCH -p defq                      # Partition name
+#SBATCH -n 8                           # Number of processor cores (i.e. tasks)
+#SBATCH -J MOF                         # Job name
+#SBATCH -p defq                        # Partition name
 ##SBATCH --mail-user=UNITYID@ncsu.edu  # Email address
 ##SBATCH --mail-type=BEGIN             # Receive email when job start
 ##SBATCH --mail-type=END               # Receive email when job end
@@ -13,9 +13,8 @@
 #SBATCH -e error.txt                   # Error file name it will be printed in the $HOME directory by default
 
 # Move into the submission directory -------------------------------------------------------------------------------
-
-
 cd $SLURM_SUBMIT_DIR
+
 # Main -------------------------------------------------------------------------------------------------------------
 rm -r initial_*
 rm -r child_*
@@ -23,4 +22,3 @@ rm -r child_*
 python mofMain.py --input input_file.yaml --cpus 8
 
 ##stress-ng --cpu $SLURM_CPUS_ON_NODE --timeout 60s --metrics-brief
-
