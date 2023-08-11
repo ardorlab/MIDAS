252c8036370cb6b420ceff8aa185c6826da3560b
Grigorios Delipei <gkdelipe@node026.cm.cluster>
2023-08-11 18:10:57 -0400

    mcycle RL developments

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -20,7 +20,166 @@ from stable_baselines3.common.monitor import Monitor
 from stable_baselines3 import SAC, PPO, A2C, DQN
 import torch as th
 
-class Gym_Env(gym.Env):
+class Cycle1_Gym_Env(gym.Env):
+    """
+    Class for wrapper adapted for Gym environment
+
+    Written by Gregory Delipei 7/12/2022
+    """
+    metadata = {'render.modes': ['console']}
+    # Define constants for clearer code
+
+    def __init__(self,solution,file_settings,fitness):
+        super(Gym_Env, self).__init__()
+        self.solution = solution
+        self.best_solution = solution
+        self.nrow=solution.nrow
+        self.fitness= fitness
+        self.ncol=solution.ncol
+        self.action_lower=-1
+        self.action_upper=1
+        self.symmetry=solution.symmetry
+        self.core_dict={}
+        self.core_dict['core_map'] = solution.core_dict['core_map']
+        self.core_dict['core_id'] = solution.core_dict['core_id']
+        self.core_dict['Inventory'] = solution.core_dict['Inventory']
+        self.order = file_settings['optimization']['order']
+        self.restart = file_settings['optimization']['restart']
+        self.fitness_const = file_settings['optimization']['selection']['fitness_constraint']
+        state0_file = file_settings['optimization']['start']
+        
+      #  self.random_design()
+        invent=list(self.core_dict['Inventory'].keys())
+        nass = len(invent)
+        self.counter=0
+        self.total_run=0
+        self.maxcounter = len(self.order)
+        with open(state0_file) as f:
+            self.start = yaml.safe_load(f)
+        self.solution.set_state(self.start)
+        self.action_type= file_settings['optimization']['stable_baselines3_options']['action_space']
+        self.observation_type = file_settings['optimization']['stable_baselines3_options']['observation_space']
+        if self.action_type=='discrete':
+            self.cmap={}
+            cmap_range=np.arange(0,nass)
+            for i in range(nass):
+                self.cmap[invent[i]]=cmap_range[i]
+            self.action_space = spaces.Discrete(nass)
+        else:
+            self.cmap={}
+            cmap_range=np.linspace(-1,1,nass)
+            for i in range(nass):
+                self.cmap[invent[i]]=cmap_range[i]
+            self.action_space = spaces.Box(low=-1, high=1,
+                                            shape=(1,), dtype=np.float32)
+        
+        if self.observation_type=='multi_discrete':
+            self.observation_space = spaces.MultiDiscrete([nass,nass,nass,nass,nass,nass,
+                                                      nass,nass,nass,nass,nass,nass,
+                                                      nass,nass,nass,nass,nass,nass,
+                                                      nass,nass,nass,nass,nass,nass,
+                                                      nass,nass,nass,nass,nass,nass,
+                                                      nass,31])
+        else:
+            self.observation_space = spaces.Box(low=-1.0, high=1.0, shape=(2,len(self.start)), dtype=np.float32)
+        
+
+        self.trackfile='trackfile.txt'
+        with open(self.trackfile, "w") as ofile:
+            ofile.write('PWR core optimization with MOF')
+
+    def reset(self):
+        """
+        Important: the observation must be a numpy array
+        :return: (np.array) 
+        """
+        self.counter=0
+        if self.restart:
+            new_state = {}
+            for key, value in self.best_solution.core_dict['fuel'].items():
+                new_state[key]=value['Value']
+            with open('state1.yml', 'w') as outfile:
+                yaml.dump(new_state, outfile, default_flow_style=False)
+            self.solution.set_state(new_state)
+        else:
+            self.solution.set_state(self.start)
+        
+        if self.observation_type=="continuous":
+            cstate = self.solution.get_mapstate(self.cmap,self.observation_type)
+            loc_state = np.zeros(cstate.shape[0])
+            loc_state[self.counter]=1
+            rstate=np.r_[[cstate],[loc_state]]
+        elif self.observation_type=="multi_discrete":
+            cstate = self.solution.get_mapstate(self.cmap,self.observation_type)
+            cstate[-1] = self.counter
+            rstate=cstate
+        return(rstate)
+
+    def step(self, action):
+        loc = self.order[self.counter]
+        act=self.solution.get_actions()
+        sact={'Location': loc,
+                'Value': action,
+                'Space':self.action_type,
+                'Action_Map':self.cmap}
+        self.solution.mapaction(sact)
+
+        if self.counter==len(self.start)-1:
+            mid = 0
+            self.total_run +=1
+            self.solution.name = "solution_{}".format(self.total_run)
+            self.solution.evaluate()
+            solList = self.fitness.calculate([self.solution])
+            self.solution=solList[0]
+            if self.fitness_const:
+                self.solution.fitness=self.solution.get_fitness()
+            reward = self.solution.fitness
+            info={'cycle_length':self.solution.parameters["cycle_length"]['value'],
+                  'max_boron':self.solution.parameters["max_boron"]['value'],
+                  'FDeltaH':self.solution.parameters["FDeltaH"]['value'],
+                  'PinPowerPeaking':self.solution.parameters["PinPowerPeaking"]['value'],
+                  'State': self.solution.get_mapstate(self.cmap,self.observation_type)}
+            all_values = open('all_value_tracker.txt','a')
+            all_values.write(f"{self.total_run},   {self.solution.name},   ")
+            for param in self.solution.parameters:
+                all_values.write(f"{self.solution.parameters[param]['value']},    ")
+            all_values.write(f"{self.solution.fitness},    ")
+            all_values.write('\n')
+            all_values.close()
+            if self.total_run ==1:
+                self.best_solution=copy.deepcopy(self.solution)
+            else:
+                if self.best_solution.fitness<self.solution.fitness:
+                    self.best_solution = copy.deepcopy(self.solution)
+        else:
+            mid = self.counter + 1
+            reward=0
+            info={}
+        self.counter+=1
+        done = bool(self.counter==len(self.order))
+        
+        if self.observation_type=="continuous":
+            cstate = self.solution.get_mapstate(self.cmap,self.observation_type)
+            loc_state = np.zeros(cstate.shape[0])
+            loc_state[mid]=1
+            rstate=np.r_[[cstate],[loc_state]]
+        elif self.observation_type=="multi_discrete":
+            cstate = self.solution.get_mapstate(self.cmap,self.observation_type)
+            cstate[-1] = mid
+            rstate=cstate
+        return((rstate, reward, done, info))
+    
+    def render(self, mode='console'):
+        if mode != 'console':
+            mode='console'
+        with open(self.trackfile, 'a') as ofile:
+            ofile.write("\nStep {}".format(self.counter))
+            ofile.write('\n    obs=' + str(self.solution.get_mapstate()) +  '\n    reward=' + str(self.solution.fitness) + "\n")
+
+    def close(self):
+        pass
+
+class Mcycle_Gym_Env(gym.Env):
     """
     Class for wrapper adapted for Gym environment
 
@@ -250,7 +409,8 @@ class Reinforcement_Learning(object):
         foo.name = "solution"
         foo.parameters = copy.deepcopy(self.file_settings['optimization']['objectives'])
         foo.add_additional_information(self.file_settings)
-        env = Monitor(Gym_Env(foo,self.file_settings,self.fitness),log_dir,info_keywords=info_kwd)
+        Custom_Env = globals()[self.file_settings['optimization']['environment']]
+        env = Monitor(Custom_Env(foo,self.file_settings,self.fitness),log_dir,info_keywords=info_kwd)
         env = DummyVecEnv([lambda: env])
         net1 = self.file_settings['optimization']['stable_baselines3_options']['policy_net']
         net2 = self.file_settings['optimization']['stable_baselines3_options']['qvalue_net']
@@ -309,7 +469,8 @@ class Reinforcement_Learning(object):
                     tensorboard_log=tens_log,policy_kwargs=policy_kwargs)
 
         games_numbers = self.generation.total
-        model.learn(total_timesteps=31*games_numbers)
+        steps_per_game = len(self.file_settings['optimization']['order'])
+        model.learn(total_timesteps=steps_per_game*games_numbers)
         model.save(model_save)
         obs = env.reset()
 
