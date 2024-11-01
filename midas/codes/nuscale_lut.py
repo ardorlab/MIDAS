186621274ad02f5c49ed51aeddb0a7f3dc367d85
Cole <cahowar9@ncsu.edu>
2024-11-01 17:50:34 -0400

    Finish preliminary Nuscale SMR Code Interface

--- midas/codes/nuscale_lut.py
+++ midas/codes/nuscale_lut.py
@@ -1,17 +1,31 @@
 import h5py
 import os
+import numpy as np
+import logging
 
-def evaluate(solution, input): #!TODO: Put parameters in docstring
+def evaluate(solutions, input): #!TODO: Put parameters in docstring
     """
     This function will find the output parameters for the NuScale SMR based on the SMR database of loading pattern calculations
+    and write them into a dictionary in the solution.parameters object
 
     Written by Cole Howard. 10/29/2024
     """
-    for soln in solution:
-        pass
+    for soln in solutions:
+        #Each objective is stored as one index in a single array withon the hdf5 file, so I am getting each specific value
+        objectives, BU = read_hdf5(solutions)
+        soln.parameters['cycle_length'] = objectives[0]
+        soln.parameters['fdeltah'] = objectives[1]
+        soln.parameters['pinpowerpeaking'] = objectives[2]
+        soln.parameters['max_boron'] = objectives[3]
+        soln.parameters['cycle_cost'] = objectives[4]
 
-    return solution
-# parameters 
+        # Adding in burnup parameters in case it is used later on
+        soln.parameters['max_burnup'] = max(BU)
+        soln.parameters['min_burnup'] = min(BU)
+        soln.parameters['average_burnup'] = np.mean(BU)
+
+    return solutions
+
 def read_hdf5(individual):
     """
     This function will search the hdf5 files containing the NuScale SMR database of loading patterns for the user-defined loading pattern,
@@ -24,19 +38,19 @@ def read_hdf5(individual):
     Written by Cole Howard. 10/29/2024
     """
     assembly_name = ''.join(map(str,individual))
-    file_number = individual[1]
+    file_number = individual[1] #The number in the hdf5 file is the same as the second number in the LP array for now, may change later
     filepath = f"/cm/shared/databases/SMR_IPWR_DATABASE/Solutions_{file_number}.hdf5"
 
-    if not os.path.exists(filepath):
-        raise FileNotFoundError(f'The file Solutions_{file_number}.hdf5 does not exist. Make sure the second number in the loading pattern is between 2-7.')
+    if not os.path.exists(filepath): #Check here to make sure the file being looked up actually exists
+        raise FileNotFoundError(f'The file Solutions_{file_number}.hdf5 does not exist. Make sure all assemblies in the LP are between 2-7.')
     
     with h5py.File(filepath,'r') as hdf5_file:
         if assembly_name in hdf5_file:
 
             assembly = hdf5_file[assembly_name]
 
-            objectives = assembly["Objectives"][:]
-            BU = assembly["BU"][:]
+            objectives = assembly["Objectives"][:] #Store all objectives for that LP
+            BU = assembly["BU"][:] #Store burnup for each assembly in the LP
             return objectives, BU
         else:
             raise KeyError(f'The assembly "{assembly_name}" was not found in the file Solutions_{file_number}.hdf5. Please check that the assembly map was typed correctly.')
