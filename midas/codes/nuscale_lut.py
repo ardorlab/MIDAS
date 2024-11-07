eda4f87759fbc5c73d388ca1aec1cf21aa8e1cdb
Cole <cahowar9@ncsu.edu>
2024-11-07 10:01:27 -0500

    Reset nuscale interface for current version of database

--- midas/codes/nuscale_lut.py
+++ midas/codes/nuscale_lut.py
@@ -12,12 +12,12 @@ def evaluate(solutions, input): #!TODO: Put parameters in docstring
     """
     for soln in solutions:
         #Each objective is stored as one index in a single array withon the hdf5 file, so I am getting each specific value
-        objectives, cost, BU = read_hdf5(solutions)
+        objectives, BU = read_hdf5(solutions) #TODO!: Add cost back in
         soln.parameters['cycle_length'] = objectives[0]
         soln.parameters['fdeltah'] = objectives[1]
         soln.parameters['pinpowerpeaking'] = objectives[2]
         soln.parameters['max_boron'] = objectives[3]
-        soln.parameters['cycle_cost'] = cost
+        #soln.parameters['cycle_cost'] = cost
 
         # Adding in burnup parameters in case it is used later on
         soln.parameters['max_burnup'] = max(BU)
@@ -38,7 +38,7 @@ def read_hdf5(individual):
     Written by Cole Howard. 10/29/2024
     """
     assembly_name = ''.join(map(str,individual))
-    file_number = f'{individual[0]}{individual[1]}' #The number in the hdf5 file is the same as the second number in the LP array for now, may change later
+    file_number = f'{individual[1]}' #The number in the hdf5 file is the same as the second number in the LP array for now, will change to first and second
     filepath = f"/cm/shared/databases/SMR_IPWR_DATABASE/Solutions_{file_number}.hdf5"
 
     if not os.path.exists(filepath): #Check here to make sure the file being looked up actually exists
@@ -50,8 +50,8 @@ def read_hdf5(individual):
             assembly = hdf5_file[assembly_name]
 
             objectives = assembly["Objectives"][:] #Store all objectives for that LP
-            cost = assembly["Cost"] #Store cost of LP
+            #cost = assembly["cost"] #Store cost of LP #TODO!: Add cost back in once new dataase is generated
             BU = assembly["BU"][:] #Store burnup for each assembly in the LP
-            return objectives, cost, BU
+            return objectives, BU
         else:
             raise KeyError(f'The assembly "{assembly_name}" was not found in the file Solutions_{file_number}.hdf5. Please check that the assembly map was typed correctly.')
